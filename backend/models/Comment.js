0f4cf31a3d71e22e1b32b4e78abb125e481064f4
laatortuejaune <160584614+laatortuejaune@users.noreply.github.com>
2025-10-27 11:06:13 +0100

    Create Comment.js

---
+++ backend/models/Comment.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  task: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+commentSchema.pre('save', function(next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
